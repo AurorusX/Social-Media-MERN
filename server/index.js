0e23f9fc8dcfcc72c601441d8ec240c375bc5c55
AurorusX <barcaokukenu@yahoo.com>
2023-04-30 14:13:52 -0500

    Configurations setup

---
+++ server/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js"
+
+/*CONFIGURATIONS*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit:"30mb", extended:true }));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true }));
+app.use(cors());
+/*Set the directory of where our assets are. In real production it could be cloud storage*/
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+/*FILE STORAGE*/
+/* Multer is for file storage when persons upload it will be stored in public/assets */
+const storage =multer.diskStorage({
+destination:function(req,file,cb){
+    cb(null,"public/assets");
+},
+
+filename:function(req,file,cb){
+    cb(null,file.originalname)
+}
+
+});
+const upload =multer({storage});
+
+/*ROUTES WITH FILES*/
+app.post("/auth/register",upload.single("picture"),register);
+
+
+
+/*DATABASE (MONGOOSE) SETUP*/
+const PORT =process.env.PORT || 6001;
+
+mongoose.
+    connect(process.env.MONGO_URL, {
+
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }).then(()=>{
+        app.listen(PORT,()=>console.log(`Server Port: ${PORT} `));
+    }).catch((error)=>console.log(`${error} did not connect`));
+
+
