06b22ee9a56f4901c6bd918eaa8b2a68f2e6cf7e
AurorusX <barcaokukenu@yahoo.com>
2023-05-02 12:07:51 -0500

    Added more changes in relation authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -12,7 +12,7 @@ export const register = async (req, res) => {
                 email,
                 password,
                 picturePath,
-                firends,
+                friends,
                 location,
                 occupation
 
@@ -28,7 +28,7 @@ export const register = async (req, res) => {
                     email,
                     password:passwordHash,
                     picturePath,
-                    firends,
+                    friends,
                     location,
                     occupation,
                     viewedProfile: Math.floor(Math.random()* 10000),
@@ -43,3 +43,25 @@ export const register = async (req, res) => {
 
 
 };
+
+/* LOGGING IN */
+
+export const login = async (req,res)=>{
+    try{
+        const { email,password}=req.body;
+        const user= await User.findOne({email:email});
+        if(!user) return res.status(400).json({msg:"User does not exist."})
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) return res.status(400).json({msg:"Invalid credentials"});
+
+    
+     const token =jwt.sign({id:user._id},process.env.JWT_SECRET);
+     delete user.password;
+     res.status(200).json({token,user});
+
+     
+    } catch (err){res.status(500).json({error: err.message});}
+
+}
+
--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import {register} from "./controllers/auth.js"
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 /*CONFIGURATIONS*/
 const __filename = fileURLToPath(import.meta.url);
@@ -42,7 +43,8 @@ const upload =multer({storage});
 /*ROUTES WITH FILES*/
 app.post("/auth/register",upload.single("picture"),register);
 
-
+/*ROUTES*/
+app.use("/auth",authRoutes);
 
 /*DATABASE (MONGOOSE) SETUP*/
 const PORT =process.env.PORT || 6001;
