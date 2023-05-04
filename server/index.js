40729fa530bef82366be44fba93120e714d3aac4
AurorusX <barcaokukenu@yahoo.com>
2023-05-03 20:06:50 -0500

    Adding and deleting friends and adding posts

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from ".middleware/auth.js";
+
+import {createPost} from "./controllers/post.js";
 
 /*CONFIGURATIONS*/
 const __filename = fileURLToPath(import.meta.url);
@@ -23,6 +29,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extended:true }));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true }));
 app.use(cors());
+
 /*Set the directory of where our assets are. In real production it could be cloud storage*/
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
 
@@ -42,9 +49,12 @@ const upload =multer({storage});
 
 /*ROUTES WITH FILES*/
 app.post("/auth/register",upload.single("picture"),register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /*ROUTES*/
 app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
+app.use("/posts",postRoutes);
 
 /*DATABASE (MONGOOSE) SETUP*/
 const PORT =process.env.PORT || 6001;
