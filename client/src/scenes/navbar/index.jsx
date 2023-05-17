41888777a8aa9605ed5ea3346ad636feb6dd919a
AurorusX <barcaokukenu@yahoo.com>
2023-05-17 01:27:33 -0500

    finalizong login and registration

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -45,7 +45,7 @@ const NavBar =  () => {
                     },
                 }}
             >
-                Socialpedia
+                Sociopedia
         </Typography>
         {isNonMobileScreens && (
             <FlexBetween backgroundColor ={neutralLight} borderRadius="9px" gap="3rem" padding ="0.1rem 1.5rem">
